41dde26506228b2f4edb42a9027c5b83a9797c5b
Ovenoboyo <ovenoboyo@gmail.com>
2020-02-02 14:51:25 +0530

    Add Fab icons to show social media links
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- app/src/main/java/com/example/techela/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/techela/ui/home/HomeFragment.java
@@ -1,5 +1,8 @@
 package com.example.techela.ui.home;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +25,51 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        com.github.clans.fab.FloatingActionButton fab_instagram = root.findViewById(R.id.fab_insta);
+        com.github.clans.fab.FloatingActionButton fab_website = root.findViewById(R.id.fab_website);
+        com.github.clans.fab.FloatingActionButton fab_facebook = root.findViewById(R.id.fab_fb);
+
+        fab_instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.instagram.com/t_e_c_h_e_l_a/");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.instagram.android");
+
+                try {
+                    startActivity(likeIng);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://www.instagram.com/t_e_c_h_e_l_a/")));
+                }
+            }
+        });
+
+        fab_facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.facebook.com/@techela.csit");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.instagram.android");
+
+                try {
+                    startActivity(likeIng);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://www.facebook.com/@techela.csit")));
+                }
+            }
+        });
+
+        fab_website.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("Website link goes here")));
+            }
+        });
         RecyclerView recyclerView = root.findViewById(R.id.eventsView);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(mLayoutManager);
