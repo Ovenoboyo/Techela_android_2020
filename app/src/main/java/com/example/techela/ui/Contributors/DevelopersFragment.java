79cd177ca3b016b01c16500a442a0dbe026fab86
Ovenoboyo <ovenoboyo@gmail.com>
2020-02-03 16:59:44 +0530

    Add page for developers
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- app/src/main/java/com/example/techela/ui/Contributors/DevelopersFragment.java
+++ app/src/main/java/com/example/techela/ui/Contributors/DevelopersFragment.java
@@ -1,13 +1,27 @@
 package com.example.techela.ui.Contributors;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.techela.MainActivity;
 import com.example.techela.R;
@@ -15,18 +29,24 @@ import com.example.techela.R;
 
 public class DevelopersFragment extends PreferenceFragmentCompat {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.developers, rootKey);
+        addPreferencesFromResource(R.xml.developers);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = super.onCreateView(inflater, container, savedInstanceState);
+
         view.setBackgroundColor(Color.WHITE);
         Toolbar toolbar = ((MainActivity)getActivity()).getToolbar();
         toolbar.setTitle("Contributors");
+
         return view;
     }
 }
