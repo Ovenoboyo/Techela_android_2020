6540a390609004fbeb43f82448e5f0eec735ce5f
Ovenoboyo <ovenoboyo@gmail.com>
2020-02-03 22:49:05 +0530

    Add firebase and authentication
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- app/src/main/java/com/example/techela/SplashActivity.java
+++ app/src/main/java/com/example/techela/SplashActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.techela.Auth.LoginActivity;
+
 public class SplashActivity extends Activity {
 
     Handler handler;
@@ -17,7 +19,7 @@ public class SplashActivity extends Activity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent=new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
